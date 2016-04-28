c3a8de5768c3f5f103b44a85690d34cd1bb8773e
daniellelara <daniellelaragourgey@gmail.com>
2016-04-28 15:08:18 +0100

    fixed game'

--- js/game.js
+++ js/game.js
@@ -1,13 +1,14 @@
 $(function () {
   $('#answer').hide();
-
-  var words = ["HTML", "javascript", "Ruby", "angular", "array", "function", "class", "programming"];
+  
+  var words = ["HTML", "javascript", "ruby", "angular", "array", "function", "class", "programming", "jquery", "sinatra"];
   console.log("connected");
   var i = words.length;
 
  
   
-    $('#start').click(function(){ 
+    $('#next').click(function(){ 
+      $( "#word" ).val(''); 
       if (i > 0) {
         var word = Math.floor(Math.random() * i);
         console.log(word);
@@ -19,17 +20,24 @@ $(function () {
         $( "#answer" ).submit(function( event ) {
           event.preventDefault();
           console.log("submitted");
-          if ($( "input" ).val() == words[word]) {
-            $('#start').html('next');
+          if ($( "#word" ).val() == words[word]) {
+            $('#next').show();
+            console.log($( "#word" ).val());
+            $('#start').html('');
+            $('#next').html('next');
             $('#message').html('correct');
             word = words.splice(word, 1);
             i = words.length;
             console.log(words);
+            
+          
           }
           else {
             $('#message').html('try again');
+            $('#next').hide();
 
           }
+          
         });
       }
       else {
